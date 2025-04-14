1acad1ca0588888ab8fdc0e7f37b590fabc3b867
sookraja <sookraja@sheridancollege.ca>
2025-04-13 23:08:31 -0400

    adding weatherkit implementation

---
+++ iOSFinalProject/ViewControllers/WeatherViewController.swift
@@ -0,0 +1,80 @@
+//
+//  WeatherViewController.swift
+//  iOSFinalProject
+//
+//  Created by Annette Sookraj on 2025-03-28.
+//
+
+import UIKit
+import CoreLocation
+import MapKit
+
+class WeatherViewController: UIViewController, CLLocationManagerDelegate {
+
+    let locationManager = CLLocationManager()
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var weatherLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+
+        setupMapView()
+        setupWeatherLabel()
+        configureLocationManager()
+    }
+
+    func setupMapView() {
+        mapView.frame = view.bounds
+        mapView.showsUserLocation = true
+        view.addSubview(mapView)
+    }
+
+    func setupWeatherLabel() {
+        weatherLabel.frame = CGRect(x: 20, y: 50, width: view.frame.width - 40, height: 50)
+        weatherLabel.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        weatherLabel.textColor = .white
+        weatherLabel.textAlignment = .center
+        weatherLabel.layer.cornerRadius = 10
+        weatherLabel.clipsToBounds = true
+        view.addSubview(weatherLabel)
+    }
+
+    func configureLocationManager() {
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else { return }
+
+        let region = MKCoordinateRegion(center: location.coordinate,
+                                        span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+        mapView.setRegion(region, animated: true)
+
+        fetchWeather(lat: location.coordinate.latitude, long: location.coordinate.longitude)
+        locationManager.stopUpdatingLocation()
+    }
+
+    func fetchWeather(lat: Double, long: Double) {
+        let apiKey = "35d5aa775f94458ebb2221915250504"
+        let urlString = "https://api.weatherapi.com/v1/current.json?key=\(apiKey)&q=\(lat),\(long)"
+
+        guard let url = URL(string: urlString) else { return }
+
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data, error == nil else { return }
+
+            do {
+                let weatherData = try JSONDecoder().decode(WeatherAPIResponse.self, from: data)
+                DispatchQueue.main.async {
+                    self.weatherLabel.text = "\(weatherData.current.temp_c)°C - \(weatherData.current.condition.text)"
+                }
+            } catch {
+                print("Failed to decode WeatherAPI data: \(error)")
+            }
+        }
+        task.resume()
+    }
+}
