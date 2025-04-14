cffabe6e21cdb6df87789a726b3fbe6c92e6bd30
BLNDM-99 <blendmustafa71@gmail.com>
2025-04-14 02:55:10 -0400

    Update CityFactsViewController.swift

--- iOSFinalProject/ViewControllers/CityFactsViewController.swift
+++ iOSFinalProject/ViewControllers/CityFactsViewController.swift
@@ -2,18 +2,32 @@
 //  CityFactsViewController.swift
 //  iOSFinalProject
 //
-//  Created by Default User on 4/14/25.
+//  Created by Blend Mustafa on 4/14/25.
 //
 
 import UIKit
+import WebKit
 
-class CityFactsViewController: UIViewController {
+class CityFactsViewController: UIViewController, WKNavigationDelegate {
 
+    @IBOutlet var webView: WKWebView!
+    @IBOutlet var activity: UIActivityIndicatorView!
+    var cityName: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let encodedCityName = cityName?.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+        
+        
+            let urlAddress = URL(string: "https://www.google.com/search?q=\(encodedCityName)")
+            let url = URLRequest(url: urlAddress!)
+            webView.load(url)
+            webView.navigationDelegate = self
     }
+    
+    
     
 
     /*
