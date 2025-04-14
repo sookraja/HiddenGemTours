96debf2b4833df65ef0e026c5d59d7ccfc8cbf00
BLNDM-99 <blendmustafa71@gmail.com>
2025-04-14 02:57:32 -0400

    Update Validator.swift

--- iOSFinalProject/Classes/Validator.swift
+++ iOSFinalProject/Classes/Validator.swift
@@ -2,7 +2,17 @@
 //  Validator.swift
 //  iOSFinalProject
 //
-//  Created by Default User on 4/14/25.
+//  Created by Blend Mustafa on 4/13/25.
 //
 
 import Foundation
+
+class Validator {
+    static func isValidEmail(_ email: String) -> Bool {
+        return email.contains("@") && email.contains(".")
+    }
+
+    static func isValidPassword(_ password: String) -> Bool {
+        return password.count >= 8
+    }
+}
