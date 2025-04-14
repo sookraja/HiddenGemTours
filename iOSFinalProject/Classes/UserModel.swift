a9d4dbd4b810e9cf54e1163550bc2ad3c1da1c2f
BLNDM-99 <blendmustafa71@gmail.com>
2025-04-14 06:48:04 +0000

    Adding my stuff

---
+++ iOSFinalProject/Classes/UserModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserModel.swift
+//  iOSFinalProject
+//
+//  Created by Default User on 4/14/25.
+//
+
+import Foundation
---
+++ iOSFinalProject/Classes/Validator.swift
@@ -0,0 +1,8 @@
+//
+//  Validator.swift
+//  iOSFinalProject
+//
+//  Created by Default User on 4/14/25.
+//
+
+import Foundation
