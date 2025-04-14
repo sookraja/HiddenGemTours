1507e446065f0f9245889b69e3a6c48046352eb9
ItsPapo <cacarlos@sheridancollege.ca>
2025-04-14 00:16:30 -0400

    🗺️ Initial commit of tour creation flow

---
+++ Classes/NewTourStopCell.swift
@@ -0,0 +1,14 @@
+//
+//  NewTourStopCell.swift
+//  iOSFinalProject
+//
+//  Created by Carlos Castro on 2025-04-13.
+//
+
+
+import UIKit
+
+class NewTourStopCell: UITableViewCell {
+    @IBOutlet weak var stopTitleLabel: UILabel!
+    @IBOutlet weak var coordinateLabel: UILabel!
+}
